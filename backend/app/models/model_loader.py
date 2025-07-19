90dd94bd2f7ea36656974e913416ae642510eb56
JeffreyIsm <globalmonkey100@gmail.com>
2025-07-19 12:41:24 +0800

    updated

--- backend/app/models/model_loader.py
+++ backend/app/models/model_loader.py
@@ -1,9 +1,6 @@
 import os
 from tensorflow.keras.models import load_model
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-MODEL_PATH = os.path.join(BASE_DIR, 'ml_model', 'best_model.h5')
-
-print("MODEL_PATH:", MODEL_PATH)
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'best_model.h5')
 
 model = load_model(MODEL_PATH)
